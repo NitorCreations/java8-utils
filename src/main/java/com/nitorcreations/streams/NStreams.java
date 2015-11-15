840fdc701b3e26ef1d9deae79653a9940fc484de
Reko Jokelainen <reko.jokelainen@nitorcreations.com>
2015-11-15 14:22:10 +0200

    feat(streams): iterator and optional to stream

---
+++ src/main/java/com/nitorcreations/streams/NStreams.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015- Nitor Creations Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.nitorcreations.streams;
+
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public final class NStreams {
+    private NStreams() { /** prevent instantiation */}
+
+    public static <T> Stream<T> asStream(Optional<T> optional) {
+        return optional.map(Stream::of).orElse(Stream.empty());
+    }
+
+    public static <T> Stream<T> asStream(Iterator<T> iterator) {
+        return asStream(iterator, false);
+    }
+
+    public static <T> Stream<T> asStream(Iterator<T> iterator, boolean parallel) {
+        Iterable<T> iterable = () -> iterator;
+        return StreamSupport.stream(iterable.spliterator(), parallel);
+    }
+}
